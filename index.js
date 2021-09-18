89d93f4b2646a1c4a5d5d5abb2e87628795265c0
Marcus Urani <marcus.urani20@gmail.com>
2021-09-17 21:43:17 -0300

    Corrigindo o bug e finalizando as funcionalidades

--- index.js
+++ index.js
@@ -5,30 +5,35 @@ var mensagemErroDiv = document.querySelector(".mensagem__erro");
 var filmes = [""];
 
 botao.addEventListener("click", function () {
-    var filmeUrl = inputFilme.value;
-    if (filmeUrl.endsWith(".jpg")) {
-        for (var i = 0; i < filmes.length; i++) {
-            if (filmeUrl == filmes[i]) {
-                var mensagemFilmeRepetido =
-                    "<p>" + "Erro, não é possível adicionar dois itens iguais" + "</p>";
-                mensagemErroDiv.innerHTML = mensagemFilmeRepetido;
-                break;
-            } else {
-                adicionarFilmesNaTela(filmeUrl);
-                var limparErro = "<p>" + "" + "</p>";
-                mensagemErroDiv.innerHTML = limparErro;
-            }
-        }
+  var filmeUrl = inputFilme.value;
+  if (
+    filmeUrl.endsWith(".jpg") ||
+    filmeUrl.endsWith(".png") ||
+    filmeUrl.endsWith(".JPG") ||
+    filmeUrl.endsWith(".PNG") ||
+    filmeUrl.endsWith(".gif")
+  ) {
+    if (filmes.indexOf(filmeUrl) != -1) {
+      var mensagemFilmeRepetido =
+        "<p>" + "Erro, não é possível adicionar dois itens iguais" + "</p>";
+      mensagemErroDiv.innerHTML = mensagemFilmeRepetido;
     } else {
-        var mensagemErro =
-            "<p>" + "Erro, não foi possível adicionar o item. É necessário inserir uma imagem .jpg" + "</p>";
-        mensagemErroDiv.innerHTML = mensagemErro;
+      adicionarFilmesNaTela(filmeUrl);
+      var limparErro = "<p>" + "" + "</p>";
+      mensagemErroDiv.innerHTML = limparErro;
     }
-    inputFilme.value = "";
+  } else {
+    var mensagemErro =
+      "<p>" +
+      "Erro, não foi possível adicionar o item. É necessário inserir uma imagem .jpg" +
+      "</p>";
+    mensagemErroDiv.innerHTML = mensagemErro;
+  }
+  inputFilme.value = "";
 });
 
 function adicionarFilmesNaTela(filme) {
-    var elementoFilme = '<img class = "imagemFilmes" src =' + filme + ">";
-    listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme;
-    filmes.push(filme);
+  var elementoFilme = '<img class = "imagemFilmes" src =' + filme + ">";
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme;
+  filmes.push(filme);
 }
